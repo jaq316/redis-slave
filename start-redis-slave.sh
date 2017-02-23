7c85003f7d8c70efadda79e064b69f9d61ce2b01
Jacques Snyman <jacques@jacquessnyman.co.za>
2017-02-23 12:51:35 +0200

    Use named redis-master link

--- start-redis-slave.sh
+++ start-redis-slave.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #
-if [ -z "$DOCKER_REDIS_MASTER_PORT_6379_ADDR" ]; then
-	echo "DOCKER_REDIS_MASTER_PORT_6379_ADDR not defined. Did you run with -link?";
-	exit 7;
-fi
+#f [ -z "$DOCKER_REDIS_MASTER_PORT_6379_ADDR" ]; then
+#	echo "DOCKER_REDIS_MASTER_PORT_6379_ADDR not defned. Did you run with -link?";
+#	exit 7;
+#fi
 
 # exec allows docker-redis-slave to receive signals for clean shutdown
 #
 
-echo "DOCKER_REDIS_MASTER_PORT_6379_ADDR -> " $DOCKER_REDIS_MASTER_PORT_6379_ADDR
-echo "DOCKER_REDIS_MASTER_PORT_6379_PORT -> " $DOCKER_REDIS_MASTER_PORT_6379_PORT
+#cho "DOCKER_REDIS_MASTER_PORT_6379_ADDR -> " $DOCKER_REDIS_MASTER_PORT_6379_ADDR
+#cho "DOCKER_REDIS_MASTER_PORT_6379_PORT -> " $DOCKER_REDIS_MASTER_PORT_6379_PORT
 
-exec /usr/local/bin/redis-server --slaveof $DOCKER_REDIS_MASTER_PORT_6379_ADDR $DOCKER_REDIS_MASTER_PORT_6379_PORT
-Contact GitHub API Training Shop Blog About
-© 2017 GitHub, Inc. Terms Privacy Security Status Help
+exec /usr/local/bin/redis-server --slaveof redis-master #$DOCKER_REDIS_MASTER_PORT_6379_ADDR $DOCKER_REDIS_MASTER_PORT_6379_PORT
